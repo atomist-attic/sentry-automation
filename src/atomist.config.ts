c7bcefafec0a9bdd576795cb5c6146c67fbbc98b
Christian Dupuis <cd@atomist.com>
2018-05-19 13:05:49 +0200

    Add some comment about github team

--- src/atomist.config.ts
+++ src/atomist.config.ts
@@ -3,6 +3,7 @@ import { ingester } from "@atomist/automation-client/graph/graphQL";
 import * as secured from "@atomist/automation-client/secured";
 import { RollbackDeployment } from "./handlers/commands/RollbackDeployment";
 
+// GitHub team membership to check if rollback can be triggered
 const AdminTeam = "atomist-automation";
 
 export const configuration: Configuration = {
